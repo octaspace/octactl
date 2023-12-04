04da7c77607d37f03a5c0b5e52378db1e1ba9c0f
Artem Teslenko <ates@ipv6.dp.ua>
2023-12-04 23:03:22 +0200

    Make pretty table

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,3 +1,5 @@
+import { Table } from 'console-table-printer';
+
 import { Octa, KeyError, ApiError } from "@octaspace/api.js";
 import { fetchFile } from "../helpers/storage";
 
@@ -5,7 +7,28 @@ export async function displayVPNNodes() {
   try {
     const key = fetchFile("key");
     const data = await new Octa(key).getVPNNodes();
-    console.table(data);
+
+    const p = new Table({
+      columns: [
+        { name: 'node_id', title: 'Node ID', alignment: 'center' },
+        { name: 'country', title: 'Country', alignment: 'center' },
+        { name: 'speed', title: 'Speed ↑↓', alignment: 'center' },
+        { name: 'reliability', title: 'Reliability', alignment: 'center' },
+        { name: 'price', title: 'Price', alignment: 'center' }
+      ]}
+    );
+
+    data.forEach((node) => {
+      p.addRow({
+        'node_id': node['node_id'],
+        'country': node['country'],
+        'speed': `${Math.round(node['net_up_mbs'] / 1024 / 1024)} | ${Math.round(node['net_down_mbs'] / 1024 / 1024)}`,
+        'reliability': (node['reliability']).toFixed(2),
+        'price': (node['traffic_price'] / 100).toFixed(2)
+      });
+    });
+
+    p.printTable();
   } catch (err: any) {
     if (err instanceof KeyError) {
       console.log("Please Login");
